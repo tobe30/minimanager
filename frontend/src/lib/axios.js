81ee6add29c4fab72ab25f1d45d3ccc47b13f809
Tobe Marizu <tobemarizu@gmail.com>
2026-01-28 01:28:39 +0100

    second commit

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
-import axios from "axios"
+import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:5000/api", // or process.env.REACT_APP_API_URL for CRA
-  withCredentials: true,
+    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
+    withCredentials: true,
 })
